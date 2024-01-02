8f2596f42875d5e8a07d11e9e40ae5a22aa17c91
nitish200899 <nitishkesarwani20@gmail.com>
2024-01-02 23:02:18 +0530

    Add user-specific welcome message and create note button

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,36 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
+    const {user} = useUser()
     return(
-        <div>
-            This is a protected route
+        <div 
+            className="h-full flex flex-col justify-center items-center space-y-4"
+        >
+           <Image 
+                src="/empty.png"
+                alt="Empty"
+                width="300"
+                height="300"
+                className="dark:hidden"
+            />
+            <Image 
+                src="/empty-dark.png"
+                alt="Empty"
+                width="300"
+                height="300"
+                className=" hidden dark:block"
+            />
+            <h2 className="text-lg font-medium">
+                Welcome to {user?.firstName}&apos;s Jotion
+            </h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2"/>
+                Create a note
+            </Button>
         </div>
     )
 }
