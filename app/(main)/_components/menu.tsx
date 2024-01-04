8c7af43fbcd48e63af2f81a37cdfca2c07e2b5f8
nitish2008 <nitishkesarwani20@gmail.com>
2024-01-05 03:11:08 +0530

    Added Banner and menu Icon

---
+++ app/(main)/_components/menu.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { useMutation } from "convex/react"
+import { useRouter } from "next/navigation"
+import { 
+    DropdownMenu,
+    DropdownMenuContent ,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+    DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu"
+import { useUser } from "@clerk/clerk-react"
+import { MoreHorizontal, Trash } from "lucide-react"
+import { toast } from "sonner"
+import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton"
+
+interface MenuProps {
+    documentId : Id<"documents">
+}
+
+export const Menu = ({documentId} : MenuProps) => {
+    const {user} = useUser()
+    const router = useRouter()
+    const archive = useMutation(api.documents.archive)
+
+    const onArchive = () => {
+        const promise = archive({id : documentId})
+
+        toast.promise(promise,{
+            loading : "Moving document to trash...",
+            success : "Document moved to trash",
+            error : "Error while moving a document to trash"
+        })
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild >
+                <Button size="sm" variant="ghost">
+                    <MoreHorizontal className="h-4 w-4"/>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className="w-60" 
+                align="end" 
+                alignOffset={8} 
+                forceMount
+            >
+                <DropdownMenuItem onClick={onArchive}>
+                    <Trash className="h-4 w-4 mr-2"/>
+                    Delete
+                </DropdownMenuItem>
+                <DropdownMenuSeparator/>
+                <div className="text-xs text-muted-foreground p-2">
+                    Last edited by {user?.fullName}
+                </div>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+Menu.Skeleton = function MenuSkeleton(){
+    return(
+        <Skeleton className="h-6 w-6"/>
+    )
+}
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,8 @@ import { Title}  from "./title"
 import { useQuery } from "convex/react"
 import { MenuIcon } from "lucide-react"
 import { useParams } from "next/navigation"
+import { Banner } from "./banner"
+import { Menu } from "./menu"
 
 interface NavbarProps {
     isCollapsed: boolean
@@ -23,8 +25,11 @@ const Navbar = ({
 
     if (document === undefined) {
         return (
-            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
                 <Title.Skeleton/>
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton/>
+                </div>
             </nav>
         );
     }
@@ -44,8 +49,12 @@ const Navbar = ({
                 )}
                 <div className="flex items-center justify-between w-full">
                    <Title initialData={document}/>
+                   <Menu documentId={document._id}/>
                 </div>
             </nav>
+            {document.isArchived && (
+                <Banner documentId={document?._id}/>
+            )}
         </>
     )
 }
